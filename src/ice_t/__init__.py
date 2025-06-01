5a4a8357ac0450a135659ce0972282b25e6c5db1
DrJsPBs <drj@drjlabs.com>
2025-06-01 15:51:37 -0700

    Fix missing newlines

--- src/ice_t/__init__.py
+++ src/ice_t/__init__.py
@@ -1,6 +1,4 @@
-"""
-ice-t: Autonomous high-performance template for web-app projects.
-"""
-
+"""ice-t: Autonomous high-performance template for web-app projects."""
 __version__ = "0.1.0"
-__author__ = "DrJLabs" 
+__author__ = "DrJLabs"
+
